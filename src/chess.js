136fa7e81fdfb2d56e4212a92dfecfd04d8be103
pinkank <45159765+pinkank@users.noreply.github.com>
2018-11-21 12:36:17 +0530

    chessBoardVisualization

---
+++ src/chess.js
@@ -0,0 +1,254 @@
+"using strict"
+console.log("Welcome to the world of Chess");
+
+//base class
+     class Pieces  {
+            constructor(id,position,image,color,points,isAlive)
+            {
+                this.id = id;
+                this.position = position; 
+                this.image = image;
+                this.color = color;
+                this.points = points;
+                this.isAlive = isAlive;
+            }
+
+        }
+
+// king class
+    class King extends Pieces {
+            constructor(id,position,color,isAlive) 
+            {
+                super(id,position,"King",color,"10",isAlive);
+            }
+        movement(){
+                return "King moves 1 step";
+            }
+    }
+   
+// Queen Class
+   class Queen extends Pieces {
+            constructor(id,position,color,isAlive)
+            {
+                super(color,position,"Queen",id,"9",isAlive);
+            }
+            movement(n){
+                return "Queen moves " + n +" step";
+            }
+    }
+
+// Bishop Class
+   class Bishop extends Pieces {
+        constructor(id,position,color,isAlive)
+        {
+                super(id,position,"Bishop",color,"3",isAlive);         
+        }
+        movement(n){
+                return "Bishop moves " + n +" step";
+            }
+   }
+
+// Rook Class
+   class Rook extends Pieces{
+        constructor(id,position,color,isAlive)
+        {
+            super(id,position,"Rook",color,"5",isAlive);
+        }
+        movement(n){
+                return "Rook moves " + n +" step";
+            }
+   }
+// Knight Class
+   class Knight extends Pieces{
+        constructor(id,position,color,isAlive)
+        {
+            super(id,position,"Knight",color,"3",isAlive);
+        }
+        movement(){
+                return "Knight moves 2-1/2 step";
+            }
+   }
+// Pawn Class
+  class Pawn extends Pieces
+  {
+        constructor(id,position,color,isAlive){
+            super(id,position,"Pawn",color,"1",isAlive);
+        }
+        movement(){
+                return "Pawn moves 1 step";
+            }
+  }
+
+  const pieceName = {
+      king : "king",
+      queen : "queen",
+      bishop : "bishop",
+      rook : "rook",
+      knight : "knight",
+      pawn : "pawn",
+  }
+  Object.freeze(pieceName);
+
+  const pieceColor = {
+      black : "black",
+      white : "white",
+  }
+  Object.freeze(pieceColor);
+
+  const pieceImage = {
+    king : "king.svg",
+    queen : "queen.svg",
+    bishop : "bishop.svg",
+    rook : "rook.svg",
+    knight : "knight.svg",
+    pawn : "pawn.svg",
+  }
+  Object.freeze(pieceImage);
+
+
+  rook = [];
+  bishop = [];
+  knight = [];
+  pawn = [];
+
+// Object of King Class
+  let k1 = new King("k1","1*4",pieceColor.black,"yes");
+  let k2 = new King("k2","8*5",pieceColor.white,"yes");
+
+// Object of Queen Class
+  let q1 = new Queen("q1","1*5",pieceColor.black,"yes");
+  let q2 = new Queen("q2","8*4",pieceColor.white,"yes");
+
+  for ( i in pieceName) {
+      if(i == "bishop") {
+        for( var j = 1; j < 3; j++){
+                const bIdBlack = j + 'bb';
+                bishop.push(new Bishop(bIdBlack,null,pieceColor.black,"yes"));
+                const bIdWhite = j + 'bw';
+                bishop.push(new Bishop(bIdWhite,null,pieceColor.white,"yes")); 
+            }  
+        }
+      else if ( i == "rook") {
+        for (var j = 1; j < 3 ; j++) {
+                const rIdBlack = j + 'rb';
+                const rIdWhite = j + 'rw';
+                rook.push(new Rook(rIdBlack,null,pieceColor.black,"yes"));
+                rook.push(new Rook(rIdWhite,null,pieceColor.white,"yes"));
+        }
+      }
+      else if ( i == "knight") {
+          for(var j =1; j < 3; j++){
+                const kIdBlack = j + 'kb';
+                const kIdWhite = j + 'kw';
+                knight.push(new Knight(kIdBlack,null,pieceColor.black,"yes"));
+                knight.push(new Knight(kIdWhite,null,pieceColor.white,"yes"));
+            }   
+      }
+      /*else if (i == "pawn") {
+          for (var j = 1; j < 9; j++) {
+                  const pIdWhite = j + 'pw';
+                  const pIdBlack = j + 'pb';
+                  const position = '2 * ' + j;
+                  pawn.push(new Pawn(pIdWhite,position,pieceColor.white,"yes"));
+                  pawn.push(new Pawn(pIdBlack,position,pieceColor.black,"yes"));
+          }
+      }*/
+  }
+
+  
+  
+  
+function insertImage( pName, color) {
+    const sourceId = "./pieces/";
+    return sourceId + pieceColor[color] + pieceImage[pName];
+}
+
+  
+function chessDisplay() {
+    var table = document.getElementById("chessboard");
+   
+    for (var i = 1; i < 9; i++) {
+        var rows = table.insertRow();
+        for (let j = 1; j < 9; j++) {
+            var cell = rows.insertCell();
+            var img = document.createElement('img');
+            img.style.height = '70px';
+            if ((i + j) % 2)
+                cell.style.backgroundColor = '#3B240B';
+            else
+                cell.style.backgroundColor = '#FBF5EF';
+            if(i == 2 || i == 7) {
+                if(i%2==0){
+                    var imageTemp = insertImage(pieceName.pawn,pieceColor.black); 
+                    const pIdBlack = j + 'pb';
+                    const position = i + ' * ' + j;
+                    pawn.push(new Pawn(pIdBlack,position,pieceColor.black,"yes"));
+                }
+                else{
+                    var imageTemp = insertImage(pieceName.pawn,pieceColor.white);
+                    const pIdBlack = j + 'pw';
+                    const position = i + ' * ' + j;
+                    pawn.push(new Pawn(pIdBlack,position,pieceColor.white,"yes"));
+                }
+                img.src = imageTemp;
+                cell.appendChild(img);
+            }
+            else if(i == 1 || i ==8) {
+                if( j==1 || j==8){
+                    if(i<2)
+                        var imageTemp = insertImage(pieceName.rook,pieceColor.black);
+                    
+                    else
+                        var imageTemp = insertImage(pieceName.rook,pieceColor.white);
+                    img.src = imageTemp;
+                    cell.appendChild(img);
+                }
+                else if( j == 2 || j == 7 ){
+                    if(i<j)
+                        var imageTemp = insertImage(pieceName.knight,pieceColor.black);
+                    else
+                        var imageTemp = insertImage(pieceName.knight,pieceColor.white);
+                    
+                    img.src = imageTemp;
+                    cell.appendChild(img);
+                }
+                else if( j == 3 || j == 6 ){
+                    if(i<j)
+                        var imageTemp = insertImage(pieceName.bishop,pieceColor.black);
+                    else
+                        var imageTemp = insertImage(pieceName.bishop,pieceColor.white);
+                    
+                    img.src = imageTemp;
+                    cell.appendChild(img);  
+                }
+                else if( j == 4 || j == 5 ){
+                    if((i+j)%2==0){
+                        if(i<j)
+                            var imageTemp = insertImage(pieceName.king,pieceColor.black);
+                        else
+                            var imageTemp = insertImage(pieceName.king,pieceColor.white);
+                        img.src = imageTemp;
+                        cell.appendChild(img); 
+                    }
+                    else{
+                        if(i<j)
+                            var imageTemp = insertImage(pieceName.queen,pieceColor.black);
+                        else
+                            var imageTemp = insertImage(pieceName.queen,pieceColor.white);
+                        img.src = imageTemp;
+                        cell.appendChild(img); 
+                    }
+                }
+            }
+            
+            
+        }
+    }
+    console.dir(pawn);
+}
+
+console.dir(bishop);
+console.dir(knight);
+
+
+
